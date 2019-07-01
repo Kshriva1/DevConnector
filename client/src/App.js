e1d7dcd983bc5eb6625bda23f31f6a7d637eb558
kshriva1 <kundan0395@gmail.com>
2019-07-01 16:26:33 -0400

    alert reducer action and types

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,12 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import './App.css';
+//Redux
+import  { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => (
+	<Provider store={store}>
    <Router>
    <Fragment>
      <Navbar />
@@ -18,7 +22,8 @@ const App = () => (
        </Switch> 
      </section>
    </Fragment>
-   </Router>  
+   </Router>
+   </Provider>  
   ) 
 
 export default App;
